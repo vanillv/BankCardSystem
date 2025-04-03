7f919fffa4aa6f3d42b2b8a4767a470bdf73f9f1
vanil <vanillvanish@gmail.com>
2025-04-03 14:55:31 +0300

    Добавил шифрование и создание логов в базе данных

--- card-management/src/main/java/com/bank/card/converter/CardNumberEncryptor.java
+++ card-management/src/main/java/com/bank/card/converter/CardNumberEncryptor.java
@@ -1,15 +1,59 @@
 package com.bank.card.converter;
 
+import io.jsonwebtoken.security.Keys;
 import jakarta.persistence.AttributeConverter;
 
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import java.util.Base64;
+
+import javax.crypto.KeyGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
 public class CardNumberEncryptor implements AttributeConverter<String, String> {
-    @Override
-    public String convertToDatabaseColumn(String s) {
-        return "";
+    private static final String ALGORITHM = "AES/GCM/NoPadding";
+    private static final int KEY_SIZE = 256;
+    private static final int IV_LENGTH = 12;
+    private final SecretKey key;
+    private final SecureRandom secureRandom = new SecureRandom();
+
+    public CardNumberEncryptor() {
+        try {
+            KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+            keyGen.init(KEY_SIZE);
+            this.key = keyGen.generateKey();
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("Ошибка инициализации генератора ключей", e);
+        }
     }
 
     @Override
-    public String convertToEntityAttribute(String s) {
-        return "";
+    public String convertToDatabaseColumn(String attribute) {
+        try {
+            byte[] iv = new byte[IV_LENGTH];
+            secureRandom.nextBytes(iv);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, key, new GCMParameterSpec(128, iv));
+            byte[] encrypted = cipher.doFinal(attribute.getBytes());
+            return Base64.getEncoder().encodeToString(iv) + ":" + Base64.getEncoder().encodeToString(encrypted);
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка шифрования", e);
+        }
+    }
+
+    @Override
+    public String convertToEntityAttribute(String dbData) {
+        try {
+            String[] parts = dbData.split(":");
+            byte[] iv = Base64.getDecoder().decode(parts[0]);
+            byte[] encrypted = Base64.getDecoder().decode(parts[1]);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(128, iv));
+            return new String(cipher.doFinal(encrypted));
+        } catch (Exception e) {
+            throw new RuntimeException("Ошибка дешифрования", e);
+        }
     }
 }
