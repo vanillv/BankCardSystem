0f84d9694bc82824145d41757776f0df2199baaa
vanil <vanillvanish@gmail.com>
2025-04-03 13:14:10 +0300

    Создал структуру для проекта - package сервисов, контроллеров, сущностей. следующий шаг - создание классов и написание базовой логики

---
+++ card-management/src/main/java/com/bank/card/application/CardManagementApplication.java
@@ -0,0 +1,9 @@
+package com.bank.card.application;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static org.springframework.boot.SpringApplication.*;
+@SpringBootApplication
+public class CardManagementApplication {
+	public static void main(String[] args) {
+		run(CardManagementApplication.class, args);
+	}
+}
